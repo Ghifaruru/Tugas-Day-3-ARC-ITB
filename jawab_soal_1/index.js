57641729809b529b347570c010d677420d74b58e
Ghifaruru <ghifari.purnimo@gmail.com>
2024-03-06 14:04:32 +0700

    perubahan sedikit

--- jawab_soal_1/index.js
+++ jawab_soal_1/index.js
@@ -1,15 +1,18 @@
 import axios from 'axios'
+//PROGRAM UNTUK MEMBUAT SEBUAH DAD JOKE
 
-//CONTOH PROGRAM YANG MENAFAATKAN FITUR GET DARI AXIOS UNTUK MEMBUAT ANGKA RANDOM DARI 1 SAMPAI 1000000
-const Url = 'https://www.random.org/integers/?num=1&min=1&max=1000000&col=1&base=10&format=plain&rnd=new';
-
-//MENGAMBIL API UNTUK RANDOM NUMBER GENARATOR
-axios.get(Url)
+axios.get('https://icanhazdadjoke.com/', {
+  headers: {
+    //MENETAPKAN HEADER UNTUK MENERIMA DALAM FORMAT JSON
+    Accept: 'application/json',
+  },
+})
   .then(response => {
-      const randomNum = parseInt(response.data);
-      console.log('Nomor undir anda :', randomNum); //OUTPUT
-    }
-  )
-  .catch(error => {
-    console.error('Kesalahan:', error.message) //JIKA ADA SEBUAH KESALAHAN
-  })
+    const dadjoke = response.data.joke;
+    console.log(`
+    ===============================================================================
+    ${dadjoke}
+    ===============================================================================`);
+  })//OUTPUT
+  //JIKA ADA SEBUAH EROR
+  .catch(error => console.error('Error fetching dad joke:', error.message));
